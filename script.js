63e2180de6ae9c8c6458bfc792ed2b5bd80c061c
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-12 21:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const input = document.getElementById('evaluatedText');
+const h3 = document.getElementById('wordCount');
+var content = "";
+input.addEventListener('input',()=>{
+	
+	content = input.value.trim();
+	
+	h3.innerHTML = content.split(" ").length;
+})
